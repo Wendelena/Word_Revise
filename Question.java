24352c8381f5983955dbb42b94e5a67d1248f7ed
Chorch Zhu <JackDavid127@126.com>
2015-06-11 00:50:32 +0800

    Slight Correction

--- Question.java
+++ Question.java
@@ -4,8 +4,7 @@ public class Question{
 	public final String choices[];
 	public final int ans;
 	public final boolean needAssure;
-	public final boolean isNameTitle;
-	public Question(String t,String c[],Word o,int a,boolean n,boolean i){
-		title=t;choices=c;obj=o;ans=a;needAssure=n;isNameTitle=i;
+	public Question(String t,String c[],Word o,int a,boolean n){
+		title=t;choices=c;obj=o;ans=a;needAssure=n;
 	}
 }
